9756e8cd190b52e98ebc65798388ae6641d7cc0b
Cadence-Ludwig <celudwig1010@gmail.com>
2026-09-24 21:01:37 -0400

    added DNode constructors

--- DNode.cpp
+++ DNode.cpp
@@ -3,3 +3,16 @@
 //
 
 #include "DNode.h"
+DNode::DNode() {
+    song=NULL;//maybe this should initialize to new Song() instead of NULL...
+    prev=NULL;
+    next=NULL;
+}
+DNode::DNode(string s, string a, int lenmin, int lensec) {
+    song=new Song(s,a,lenmin,lensec);
+    prev=NULL;
+    next=NULL;
+}
+DNode::~DNode() {
+    
+}
